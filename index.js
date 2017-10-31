1232d8d372eede9ab650ed4db50559723106d531
Nicolás Arias <nicolas@nicolasarias.com>
2017-10-31 04:50:54 +0200

    Add options.main and fix imports

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const fs = require('fs');
 const path = require('path');
 const less = require('less');
 
-module.exports = function(statiq, options) {
+module.exports = function(statiq, options = {}) {
   const { assetHandler } = statiq._config;
 
   statiq.config({
     assetHandler: function(source, target) {
       if (path.extname(source) !== '.less') return assetHandler(source, target);
+      if (options.main && path.basename(source, '.less') !== path.basename(options.main, '.less')) return;
 
       const lessTarget = path.join(path.dirname(target), path.basename(target, '.less') + '.css');
 
-      less.render(fs.readFileSync(source, 'utf8'), options || {}, (e, output) => {
+      less.render(fs.readFileSync(source, 'utf8'), Object.assign(options, { filename: source }), (e, output) => {
         fs.writeFileSync(lessTarget, output.css);
       });
 
