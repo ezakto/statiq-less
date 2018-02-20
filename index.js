cb0906821aada4740e73a659b6bacf0a0dace84c
Nicolás Arias <nicolas@nicolasarias.com>
2018-02-21 00:42:38 +0700

    Update to statiq 0.5

--- index.js
+++ index.js
@@ -2,21 +2,33 @@ const fs = require('fs');
 const path = require('path');
 const less = require('less');
 
-module.exports = function(statiq, options = {}) {
-  const { assetHandler } = statiq._config;
+module.exports = function(options = {}) {
+  return function(statiq) {
+    const { assetHandler } = statiq._config;
 
-  statiq.config({
-    assetHandler: function(source, target) {
-      if (path.extname(source) !== '.less') return assetHandler(source, target);
-      if (options.main && path.basename(source, '.less') !== path.basename(options.main, '.less')) return;
+    statiq.config({
+      assetHandler: function(source, target) {
+        if (path.extname(source) !== '.less') return assetHandler(source, target);
+        if (options.main && path.basename(source, '.less') !== path.basename(options.main, '.less')) return;
 
-      const lessTarget = path.join(path.dirname(target), path.basename(target, '.less') + '.css');
+        const lessTarget = path.join(path.dirname(target), path.basename(target, '.less') + '.css');
 
-      less.render(fs.readFileSync(source, 'utf8'), Object.assign(options, { filename: source }), (e, output) => {
-        fs.writeFileSync(lessTarget, output.css);
-      });
+        return new Promise((resolve, reject) => {
+          fs.readFile(source, 'utf8', (err, data) => {
+            if (err) return reject(err);
 
-      return lessTarget;
-    }
-  });
+            less.render(data, Object.assign(options, { filename: source }), (err, output) => {
+              if (err) return reject(err);
+
+              fs.writeFile(lessTarget, output.css, err => {
+                if (err) return reject(err);
+
+                resolve(lessTarget);
+              });
+            });
+          });
+        });
+      }
+    });
+  }
 };
